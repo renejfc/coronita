be7c45d3efaec7a8ea5aa9c2d9b782933babc089
René <renejfc@gmail.com>
2024-03-05 20:52:01 +0000

    refactor(command): implement jsx instead of plain obj

--- src/commands/gayer.tsx
+++ src/commands/gayer.tsx
@@ -1,5 +1,5 @@
 import { SlashCommand } from "@lilybird/handlers";
-import { ApplicationCommand, UserOption } from "@lilybird/jsx";
+import { ApplicationCommand, UserOption, Embed, EmbedField, EmbedFooter, EmbedImage } from "@lilybird/jsx";
 import { randomInt } from "crypto";
 
 const command = (
@@ -9,52 +9,34 @@ const command = (
   </ApplicationCommand>
 );
 
+const embedResult = ({ firstUser, secondUser }: { firstUser: string, secondUser: string }) => {
+  const tagUser = (user: string) => `<@${user}>`;
+  const random = randomInt(2);
+  const randomUser = random === 0 ? firstUser : secondUser;
+
+  return (
+    <Embed title="🏳️‍🌈🏳️‍🌈🏳️‍🌈 CONCURSO DE JOTOS 🏳️‍🌈🏳️‍🌈🏳️‍🌈" description="Concurso a la reina del baile 💅" color={0xffc0cb}>
+      <EmbedField name="Concursante 1" value={tagUser(firstUser)} inline />
+      <EmbedField name="Concursante 2" value={tagUser(secondUser)} inline />
+      <EmbedField name="" value="-------------------" />
+      <EmbedField name="Le Ganadore" value={tagUser(randomUser)} />
+      <EmbedImage url="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd28yMm5pdGliZzNidGlpNWx2ZXR6N3hjbTNlMmVzcWNtNnM2dGtyNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oOi3roYRf4NW0/giphy.gif" />
+      <EmbedFooter text="🥵🥵🥵" />
+    </Embed>
+  );
+}
+
 export default {
   post: "GLOBAL",
   data: command,
-  run: async (interaction) => {
-    await interaction.deferReply();
+  run: async ( interaction ) => {
+    const firstUser = interaction.data.getUser("user1", true);
+    const secondUser = interaction.data.getUser("user2", true);
 
-    const firstUser = interaction.data.getUser("user1");
-    const secondUser = interaction.data.getUser("user2");
-
-    const random = randomInt(2);
-    const randomUser = random === 0 ? firstUser : secondUser;
-
-    await interaction.editReply({
+    await interaction.reply({
       embeds: [
-        {
-          title: "🏳️‍🌈🏳️‍🌈🏳️‍🌈 CONCURSO DE JOTOS 🏳️‍🌈🏳️‍🌈🏳️‍🌈",
-          description: `Concurso a la reina del baile 💅`,
-          fields: [
-            {
-              inline: true,
-              name: "Concursante 1",
-              value: `<@${firstUser}>`,
-            },
-            {
-              inline: true,
-              name: "Concursante 2",
-              value: `<@${secondUser}>`,
-            },
-            {
-              name: "",
-              value: "-------------------",
-            },
-            {
-              name: "Le Ganadore",
-              value: `<@${randomUser}>`,
-            },
-          ],
-          footer: {
-            text: `🥵🥵🥵`,
-          },
-          color: 0xffc0cb,
-          image: {
-            url: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd28yMm5pdGliZzNidGlpNWx2ZXR6N3hjbTNlMmVzcWNtNnM2dGtyNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/oOi3roYRf4NW0/giphy.gif",
-          },
-        },
-      ],
+        embedResult({ firstUser, secondUser })
+      ]
     });
   },
 } satisfies SlashCommand;
