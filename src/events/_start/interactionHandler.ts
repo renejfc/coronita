3127b62b6706d78b5aad747a92e0e8774a5af850
René <renejfc@gmail.com>
2024-11-19 13:48:56 +0100

    refactor: polished interaction-handler

--- src/events/_start/interactionHandler.ts
+++ src/events/_start/interactionHandler.ts
@@ -1,28 +1,52 @@
-import { readdirSync } from 'node:fs'
-import { join } from 'node:path'
-import { pathToFileURL } from 'node:url'
+import { readdirSync } from "node:fs"
+import { join } from "node:path"
+import { pathToFileURL } from "node:url"
 
-const ButtonsDir = join(process.cwd(), '.robo', 'build', 'buttons')
-const ModalsDir = join(process.cwd(), '.robo', 'build', 'modals')
+type Handler = (...args: any[]) => Promise<void>
 
-export const ButtonHandlers = new Map<string, any>()
-export const ModalHandlers = new Map<string, any>()
+type HandlerModule = {
+  customID: string
+  default: Handler
+}
 
+const BaseDir = join(process.cwd(), ".robo", "build")
+const ButtonsDir = join(BaseDir, "buttons")
+const ModalsDir = join(BaseDir, "modals")
+
+export const ButtonHandlers = new Map<string, Handler>()
+export const ModalHandlers = new Map<string, Handler>()
 
 // Load interaction handlers in advance for future use
 export default async () => {
   await Promise.all([
-    ...readdirSync(ButtonsDir).map(async (fileName) => {
-      const filePath = join(ButtonsDir, fileName)
-      const fileURL = pathToFileURL(filePath).href
-      const handler = await import(fileURL)
-      ButtonHandlers.set(handler.customID, handler.default)
-    }),
-    ...readdirSync(ModalsDir).map(async (fileName) => {
-      const filePath = join(ModalsDir, fileName)
-      const fileURL = pathToFileURL(filePath).href
-      const handler = await import(fileURL)
-      ModalHandlers.set(handler.customID, handler.default)
-    })
+    ...readdirSync(ButtonsDir).map((fileName) => loadHandler(join(ButtonsDir, fileName), ButtonHandlers, isHandler)),
+    ...readdirSync(ModalsDir).map((fileName) => loadHandler(join(ModalsDir, fileName), ModalHandlers, isHandler)),
   ])
+}
+
+const isHandler = (module: unknown): module is HandlerModule => {
+  return (
+    typeof module === "object" &&
+    module !== null &&
+    "customID" in module &&
+    typeof module.customID === "string" &&
+    "default" in module &&
+    typeof module.default === "function"
+  )
+}
+
+const loadHandler = async (
+  path: string,
+  handlers: Map<string, Handler>,
+  validate: (module: unknown) => module is HandlerModule,
+) => {
+  const filePath = join(path)
+  const fileURL = pathToFileURL(filePath).href
+  const handler = await import(fileURL)
+
+  if (!validate(handler)) {
+    throw new Error(`Invalid handler module: ${filePath}`)
+  }
+
+  handlers.set(handler.customID, handler.default)
 }
